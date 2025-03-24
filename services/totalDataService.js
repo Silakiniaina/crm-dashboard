defe8675bfea4408abe7a30d353972a058e82435
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-24 14:14:10 +0300

    feat : Create function getExpenseBydId in totalDataService.

--- services/totalDataService.js
+++ services/totalDataService.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const JAVA_API_URL = 'http://localhost:8080/api/data-total';
 const JAVA_API_DETAILS_URL = 'http://localhost:8080/api/data-total/details';
+const JAVA_API_EXPENSES_URL = 'http://localhost:8080/api/expenses';
 
 class TotalDataService {
     async getTotalData() {
@@ -23,6 +24,17 @@ class TotalDataService {
         } catch (error) {
             console.error(`Erreur lors de la récupération des détails pour type ${type}:`, error);
             throw new Error('Impossible de récupérer les détails des données');
+        }
+    }
+
+    async getExpenseById(id) {
+        try {
+            const response = await axios.get(`${JAVA_API_EXPENSES_URL}/${id}`);
+            console.log(`Expense with id=${id}: `, response.data);
+            return response.data;
+        } catch (error) {
+            // console.error(`Erreur lors de la récupération de l'expense avec id=${id}:`, error);
+            throw new Error('Impossible de récupérer l\'expense');
         }
     }
 }
