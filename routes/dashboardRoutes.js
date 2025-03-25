c23dc2eef40f402f66cb944f019b74a84d8c3fd3
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 21:13:12 +0300

    feat : Implement auth check for dashboard.

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const TotalDataController = require('../controllers/totalDataController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get('/dashboard', TotalDataController.getTotalData);
-router.get('/data-total/details', TotalDataController.getTotalDataDetails);
+router.get('/dashboard', authMiddleware.isAuthenticated, TotalDataController.getTotalData);
+router.get('/data-total/details', authMiddleware.isAuthenticated, TotalDataController.getTotalDataDetails);
 
 module.exports = router;
