1caeeec6ff6c7ab0da6bb65114b6f114d592e3bf
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 16:27:04 +0300

    feat : Create function getAllTickets in ticketController.

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -2,6 +2,22 @@ const TicketService = require('../services/ticketService');
 
 class TicketController{
 
+    async getAllTickets(req, res){
+        try {
+            const ticketsResponse = await TicketService.getAllTickets();
+            const tickets = ticketsResponse.data;
+            res.render('all-tickets',{
+                title : 'All tickets',
+                tickets
+            })
+        } catch (error) {
+            console.error(error);
+            res.status(500).render('error', { 
+                title: 'Error', 
+                message: 'Error while fetching all tickets' 
+            });
+        }
+    }
 
 }
 
