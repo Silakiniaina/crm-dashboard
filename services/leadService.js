5a87ddc9b1feb4d04ff3179e3e826bd8c58e6a37
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 14:32:24 +0300

    feat : Create function getAllLeads in leadService.

--- services/leadService.js
+++ services/leadService.js
@@ -3,6 +3,15 @@ const BASE_URL = 'http://localhost:8080/api/leads';
 
 class LeadService{
 
+    async getAllLeads(){
+        try {
+            const response = await axios.get(BASE_URL);
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching all leads:", error);
+            throw new Error('Unable to retrieve all leads');
+        }
+    }
 }
 
 module.exports = new LeadService();
