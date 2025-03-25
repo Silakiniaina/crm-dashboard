7c2590dee8e3ae8f0913f4ce5936b5bd2a9c6228
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 14:37:51 +0300

    feat : Create function getAllLeads in leadController.

--- controllers/leadController.js
+++ controllers/leadController.js
@@ -2,6 +2,21 @@ const LeadService = require('../services/leadService');
 
 class LeadController{
 
+    async getAllLeads(req, res){
+        try {
+            const leads = LeadService.getAllLeads().data;
+            res.render('all-leads',{
+                title : 'All leads',
+                leads
+            })
+        } catch (error) {
+            console.error(error);
+            res.status(500).render('error', { 
+                title: 'Error', 
+                message: 'Error while fetching all leads' 
+            });
+        }
+    }
 }
 
 module.exports = new LeadController();
