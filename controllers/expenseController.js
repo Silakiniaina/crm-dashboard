f286f27bc1c29f683149afb890954b6e7f819481
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 09:33:02 +0300

    refactor : Move function updateExpense(req, res) to expenseController.

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -24,6 +24,30 @@ class ExpenseController {
             });
         }
     }
+
+    async updateExpense(req, res) {
+        try {
+            const id = parseInt(req.body.id);
+            const amount = parseFloat(req.body.amount);
+            
+            await ExpenseService.updateExpense(id, amount);
+            
+            const expenseData = await ExpenseService.getExpenseById(id);
+            const type = expenseData.data.ticket ? '1' : 
+                         expenseData.data.lead ? '2' : null;
+
+            if (!type) {
+                throw new Error('Unable to determine expense type');
+            }
+
+            res.redirect(`/data-total/details?type=${type}`);
+        } catch (error) {
+            res.status(500).render('error', { 
+                title: 'Error', 
+                message: error.toString()
+            });
+        }
+    }
 }
 
 module.exports = new ExpenseController();
