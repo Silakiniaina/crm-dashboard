80c6662f43273c79c8eb94bf92d6837af1be4c44
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 09:29:04 +0300

    refactor : Move function getTotalDataDetails(req, res) to totalDataController.

--- controllers/totalDataController.js
+++ controllers/totalDataController.js
@@ -18,6 +18,47 @@ class TotalDataController {
             });
         }
     }
+
+    async getTotalDataDetails(req, res) {
+        try {
+            const type = req.query.type;
+            if (!['0', '1', '2'].includes(type)) {
+                throw new Error('Invalid type parameter');
+            }
+
+            const detailsData = await TotalDataService.getTotalDataDetails(type);
+            const details = detailsData.data;
+            let title;
+            let pageName = "budget";
+            
+            switch (type) {
+                case '0':
+                    title = 'Budget Details';
+                    pageName = "budget";
+                    break;
+                case '1':
+                    title = 'Ticket Expense Details';
+                    pageName = "expense";
+                    break;
+                case '2':
+                    title = 'Lead Expense Details';
+                    pageName = "expense";
+                    break;
+            }
+
+            res.render(pageName+'-details', { 
+                title, 
+                details,
+                type
+            });
+        } catch (error) {
+            console.error(error);
+            res.status(500).render('error', { 
+                title: 'Error', 
+                message: 'Error while fetching details' 
+            });
+        }
+    }
 }
 
 module.exports = new TotalDataController();
