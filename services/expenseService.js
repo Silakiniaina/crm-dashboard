1b22c9aad1e578703c15d7b35d6c9d861427d7fa
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 09:31:38 +0300

    refactor : Move function updateExpense(id,amount) to expenseService.

--- services/expenseService.js
+++ services/expenseService.js
@@ -12,6 +12,16 @@ class ExpenseService {
             throw new Error('Unable to retrieve expense');
         }
     }
+
+    async updateExpense(id, amount) {
+        try {
+            const response = await axios.put(`${BASE_URL}/${id}`, { amount });
+            return response.data;
+        } catch (error) {
+            console.error(`Error updating expense with id=${id}:`, error);
+            throw new Error('Unable to update expense');
+        }
+    }
 }
 
 module.exports = new ExpenseService();
