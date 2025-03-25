7d2a35c7e81f73a22ad515d955978cbf0529fd2a
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 21:11:53 +0300

    feat : Implement auth check into expense.

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const ExpenseController = require('../controllers/expenseController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get('/expense-update-details', ExpenseController.getExpenseUpdateDetails);
-router.post('/expense-update-details', ExpenseController.updateExpense);
+router.get('/expense-update-details', authMiddleware.isAuthenticated, ExpenseController.getExpenseUpdateDetails);
+router.post('/expense-update-details', authMiddleware.isAuthenticated , ExpenseController.updateExpense);
 
 module.exports = router;
