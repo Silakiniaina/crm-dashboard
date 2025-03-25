4159e125ff9c140625457fe2bf4baa93bfcf9dd6
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-03-25 16:24:35 +0300

    feat : Create function getAllTickets on ticketService.

--- services/ticketService.js
+++ services/ticketService.js
@@ -3,6 +3,15 @@ const BASE_URL = 'http://localhost:8080/api/tickets';
 
 class TicketService{
 
+    async getAllTickets(){
+        try {
+            const response = await axios.get(BASE_URL);
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching all tickets:", error);
+            throw new Error('Unable to retrieve all tickets');
+        }
+    }
 
 }
 
